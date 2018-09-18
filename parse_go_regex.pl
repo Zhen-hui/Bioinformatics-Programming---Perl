b251f01a044b1e29b6c0b7c8c98d14133cf0543e
Zhen-hui <33742038+Zhen-hui@users.noreply.github.com>
2018-09-18 08:27:42 -0700

    Use Perl Regex to parse information from Gene Ontology file

---
+++ parse_go_regex.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature 'say';
+
+# This program parses the Gene Ontology file with regular expression to extract:
+# 1. id
+# 2. name
+# 3. namespace
+# 4. def
+# 5. multiple alt_id entries
+# 6. multiple is_a entries
+
+read_GO_desc();
+
+sub read_GO_desc {
+	my $GO_desc_file = '../Module01/scratch/go-basic.obo';
+	open( GO_DESC, '<', $GO_desc_file ) or die $!;
+	
+	# Open a file handlers to store output
+	open( OUT, '>', "parsed_output.txt" ) or die $!;
+	open( STDERR, '>', "not_parsed_output.txt" ) or die $!;
+	
+	# Initialize $_ so redefinition of $/ to regex will not cause warning.
+	$_ = '';
+	local $/ = /\[Term]|\[Typedef]/;
+
+	# Read file and parse for data of interest.
+	while (my $long_GO_desc = <GO_DESC>) {
+		chomp $long_GO_desc;
+		
+		my $parsing_regex = qr/
+			^id:\s+(?<id>GO:[0-9]+)\n
+			^name:\s+(?<name>.*?)\n 
+			^namespace:\s+(?<namespace>\S+)\n 
+			^def:\s+"(?<def>.+?)"                
+		/msx;						
+		                                   
+		# Regex to get all is_a Go Terms
+		my $findIsa = qr/
+			^is_a:\s+(?<isa>.*?)\s+!
+		/msx;
+		
+		# Regex to get all alt_id Go Terms
+		my $findAltId = qr/
+			^alt_id:\s+(?<alt_id>.*?)\s+
+		/msx;
+
+		if ( $long_GO_desc =~ /$parsing_regex/ ) {
+			say OUT join ("\n", $+{id}, $+{name}, $+{namespace}, $+{def});			
+			
+			say OUT "alt_ids:";
+			my @alt_ids = ();
+			while ( $long_GO_desc =~ /$findAltId/g ) {
+				push( @alt_ids, $+{alt_id} );
+			}
+			
+			# Not all records contain alt IDs, so check first before printing.
+			if ( @alt_ids ) {
+				say OUT join( ",", @alt_ids );
+			}
+			
+			say OUT "isa:";
+			my @isas = ();
+			while ( $long_GO_desc =~ /$findIsa/g ) {
+				push( @isas, $+{isa} );
+			}
+			say OUT join( ",", @isas ), "\n";
+		}
+		else {
+			say STDERR $long_GO_desc;
+		}
+	}
+}
