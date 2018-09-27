ff8f603113d330ff22b0dd4c088799f18d888833
Zhen-hui <33742038+Zhen-hui@users.noreply.github.com>
2018-09-26 21:29:58 -0700

    Create BLAST objects using a module

--- parse_blast.pl
+++ parse_blast.pl
@@ -2,38 +2,37 @@
 use strict;
 use warnings;
 use feature 'say';
+use BLAST;
 
-read_BLAST();
+#  Uses the BLAST class to create BLAST objects
+my $BLAST_hit = read_BLAST();
+foreach my $transcript ( sort keys $BLAST_hit ) {
+	$BLAST_hit->{$transcript}->print_all();
+}
 
+# Return hash of BLASH hits
 sub read_BLAST {
 	my $BLAST_file = '/scratch/RNASeq/blastp.outfmt6';
-	open( BLAST, '<', $BLAST_file ) or die $!;
+	open( BLAST_fh, '<', $BLAST_file ) or die $!;
 
-	# Open a file handlers to store output
-	open( OUT,    '>', "parsed_BLAST_output.txt" )     or die $!;
-	open( STDERR, '>', "not_parsed_BLAST_output.txt" ) or die $!;
+	# Hash to store BLAST hits
+	my %BLAST_hit;
 
 	# Read file and parse for data of interest.
-	while ( my $line = <BLAST> ) {
-		chomp $line;
-		
-		my $parsing_regex = qr/
-			^(?<transcript>\S+?)\|
-		    (?<isoform>\w+\.+[1-9]+)?\t
-		    gi\|(?<gi>\d+?)\| 
-            sp\|(?<sp>.*?)\|
-            (?<prot>.*_SCHPO)\s  
-            (?<pident>\d+\.\d+)\s  
-            (?<length>\d+)\s 
-		    (?<mismatch>\d+)\s
-		    (?<gapopen>\d+)\s 
-		/msx;
+	while ( my $record = <BLAST_fh> ) {
+		chomp $record;
 
-		if ( $line =~ /$parsing_regex/ ) {
-			say OUT join( "\t", $+{transcript}, $+{isoform}, $+{gi}, $+{sp}, $+{prot}, $+{pident}, $+{length}, $+{mismatch}, $+{gapopen} );
-		} 
-		else {
-			say STDERR $line;
+		# Instantiate new BLAST hit with new() constructor
+		my $hit = BLAST->new();
+		
+		# Invoke object's method, parse_blast_hit, to set attributes
+		$hit->parse_blast_hit($record);
+
+		# Object must have defined 'id' attribute to be added to hash
+		if ( defined $hit->transcript() ) {
+			$BLAST_hit{ $hit->transcript() } = $hit;
 		}
 	}
+	close BLAST_fh;
+	return \%BLAST_hit;
 }
